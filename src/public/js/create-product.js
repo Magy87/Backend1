2e2f789fb27b1e36bdd887b88d2385206da21f92
Magali Moyano <magymoyano021020@gmail.com>
2024-10-08 00:36:19 -0300

    terminando

--- src/public/js/create-product.js
+++ src/public/js/create-product.js
@@ -1,2 +1,17 @@
-console.log(kjbkj
-)
+
+const createProduct = document.getElementById('create-product')
+
+createProduct.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(createProduct)
+    try {
+        fetch('/api/products', {
+            method: 'POST',
+            body: formData
+        })
+
+    } catch (error) {
+        console.los(error)
+    }
+})
+
---
+++ src/public/js/edit-product.js
@@ -0,0 +1,35 @@
+// document.addEventListener('click', async (e) => {
+//     if (e.target && e.target.classList.contains("btn-delete")) {
+//         const [, id] = e.target.id.split('-')
+//         try {
+//             await fetch(`/api/products/${id}`, {
+//                 method: 'DELETE'
+//             })
+//         } catch (error) {
+//             console.log(error)
+//         }
+
+//     }
+// })
+const deleteButtons = document.querySelectorAll('.btn-delete');
+
+deleteButtons.forEach(button => {
+    button.addEventListener('click', async (e) => {
+        const productId = e.target.dataset.id; // Asume que el botón tiene un data-id con el ID del producto
+        try {
+            const response = await fetch(`/api/products/${productId}`, {
+                method: 'DELETE'
+            });
+
+            if (response.ok) {
+                alert('Producto eliminado');
+                window.location.reload(); // Refresca la página para reflejar los cambios
+            } else {
+                const error = await response.json();
+                console.error(error);
+            }
+        } catch (error) {
+            console.error('Error eliminando el producto:', error);
+        }
+    });
+});
